db60692077db6b85c96ea4f4cec252888522751b
FacuM <facumo.fm@gmail.com>
2019-08-09 15:08:25 -0300

    Update required scripting for statistics.

--- estadisticas.php
+++ estadisticas.php
@@ -1,2 +1,6 @@
-<?php $title = 'Estadísticas'; $secure = true; require_once('includes/header.php'); ?>
+<?php
+  $title   = 'Estadísticas'; $secure = true; require_once('includes/header.php');
+  $scripts = [ 'estadisticas' ]; 
+?>
+<canvas id="stats" width="400" height="400"></canvas>
 <?php require_once('includes/footer.php'); ?>
