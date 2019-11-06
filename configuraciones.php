4b7e508d26de37a04961a2288b86eacd82227a1b
LeonelMoyano1809 <leonelmoyano1809@gmail.com>
2019-11-06 13:52:34 -0300

    empece con la parte de configuraciones

--- configuraciones.php
+++ configuraciones.php
@@ -2,7 +2,21 @@
   $title   = 'Configuraciones'; $secure = true; require_once('includes/header.php');
   $scripts = [ 'configuraciones' ]; 
 ?>
-<a class="waves-effect waves-light btn-large" href="misdatos.php"><i class="material-icons right">person</i>Mis datos</a>
-<a class="waves-effect waves-light btn-large" href="horarios.php"><i class="material-icons right">calendar_today</i>Horarios</a>
-<a class="waves-effect waves-light btn-large" href="reportes.php"><i class="material-icons right">notification_important</i>Motivos de reportes</a>
+<div class="row">
+	<a class="waves-effect waves-light btn-large" id="misdatos" onclick="misdatos()"><i class="material-icons right">person</i>Mis datos</a>
+	<a class="waves-effect waves-light btn-large" id="horarios" onclick="horarios()"><i class="material-icons right">calendar_today</i>Horarios</a>
+	<a class="waves-effect waves-light btn-large" id="reportes" onclick="reportes()"><i class="material-icons right">notification_important</i>Motivos de reportes</a>
+</div>
+
+<div class="row">
+	<div class="center-align">
+		<h5 class="col s12 grey-text" id="TituloC">Configuraciones</h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="center-align">
+		<h5 class="col s12" id="explicacion" >Elegí lo que quieras modificar</h5>
+	</div>
+</div>
 <?php require_once('includes/footer.php'); ?>
--- scripts/configuraciones.js
+++ scripts/configuraciones.js
@@ -3,4 +3,31 @@ $('document').ready(function () {
 });
 document.addEventListener('DOMContentLoaded', function() {
     $('.datepicker').datepicker();
-  });
+});
+
+function reportes(){
+	$('#reportes').attr("disabled", true);
+	$('#horarios').attr("disabled", false);
+	$('#misdatos').attr("disabled", false);
+
+	$('#TituloC').html("Editar reportes");
+	$('#explicacion').html("Editá los motivos por los cuales cancelarias un pedido");	
+}
+
+function horarios(){
+	$('#horarios').attr("disabled", true);
+	$('#reportes').attr("disabled", false);
+	$('#misdatos').attr("disabled", false);
+
+	$('#TituloC').html("Editar Horarios");
+	$('#explicacion').html("Editá los horarios de atención");	
+}
+
+function misdatos(){
+	$('#misdatos').attr("disabled", true);
+	$('#horarios').attr("disabled", false);
+	$('#reportes').attr("disabled", false);
+
+	$('#TituloC').html("Editar mis datos");
+	$('#explicacion').html("Editá tus datos personales");	
+}
