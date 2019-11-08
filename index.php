e9d218076dd45a0c3b918a1e45ad15b45b56a628
FacuM <facumo.fm@gmail.com>
2019-11-08 20:56:11 -0300

    Index: add basic download frontpage.

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<?php
- header('location: pedidos.php');
-?>
+<?php $title = 'Pedidos'; $secure = false; $showmenu = false; require_once('includes/header.php'); ?>
 
-<html>
-    <head>
-    </head>
-    <body>
-        <p>Si no estás en <b>Pedidos</b> <a href="pedidos.php">hacé clic acá</a>.
-    </body>
-</html>
+    <div id="vcentered_message" class="row valign-wrapper">
+      <div class="col s12 center-align grey-text">
+        <h5>
+          <i class="large material-icons">cloud_download</i> <br>
+            buffrapp_beta-0.1.apk
+          </h5>
+          <h6>
+            Android 4.1 o superior
+          </h6>
+          <br>
+          <a href="../buffrapp_beta-0.1.apk" class="waves-effect waves-light btn btn-flat waves-green green white-text">Descargar ahora</a>
+      </div>
+    </div>
+
+<?php require_once('includes/footer.php'); ?>
